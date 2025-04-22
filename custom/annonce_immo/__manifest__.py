372652f89e0f322f96eafb8c309cb641c293d523
Ramuo <barry.alpha88@outlook.fr>
2025-04-22 10:34:54 +0200

    Creation module

---
+++ custom/annonce_immo/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Annonce Immo",
+    "version": "1.0.0",
+    "summary": "Annonce Immobilière",
+    "sequence": 5,
+    "description": """
+        Module d'annonces immobilières pour afficher les biens disponibles.
+    """,
+    "author": "Alpha",
+    "company": "Miandano",
+    "website": "https://www.website.com",
+    "category": "Sales",
+    "depends": ['base'],
+    "data": [
+        #Security
+        'security/ir.model.access.csv',
+        #Views
+        'views/bien_view.xml',
+        'views/menu.xml',
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
---
+++ custom/annonce_immo/models/bien.py
@@ -0,0 +1,29 @@
+from odoo import models, fields
+
+class Bien(models.Model):
+    _name = "bien.immobilier"
+    _description = "Annonce de Biens Immobiliers"
+
+    name = fields.Char(string="Nom", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Code Postal")
+    date_availability = fields.Date(string="Disponible Le")
+    expected_price = fields.Float(string="Prix Attendu")
+    best_offer = fields.Float(string="Meilleure Offre")
+    selling_price = fields.Float(string="Prix de Vente")
+    bedrooms = fields.Integer(string="Chambres")
+    living_area = fields.Integer(string="Surface (m²)")
+    facades = fields.Integer(string="Façades")
+    garage = fields.Boolean(string="Garage", default=False)
+    garden = fields.Boolean(string="Jardin", default=False)
+    garden_area = fields.Integer(string="Surface Jardin")
+    garden_orientation = fields.Selection(
+        [
+            ('north', 'Nord'), 
+            ('south', 'Sud'), 
+            ('east', 'Est'), 
+            ('west', 'Ouest')
+        ],
+        string="Orientation Jardin",
+        default='north'
+    )
