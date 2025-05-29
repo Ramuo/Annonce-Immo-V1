ba8b969acaca719d190cd6d956032708e6494bf6
Ramuo <barry.alpha88@outlook.fr>
2025-05-29 11:09:12 +0200

    Access groups

--- custom/annonce_immo/__manifest__.py
+++ custom/annonce_immo/__manifest__.py
@@ -14,6 +14,7 @@
     "data": [
         #Security
         'security/ir.model.access.csv',
+        'security/res_groups.xml',
         #Views
         'views/bien_view.xml',
         'views/type_bien_view.xml',
